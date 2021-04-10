22a2ba422d38f1d3d51c276a2a769bd1c641810e
Maciej <maciejzadworny@gmail.com>
2021-04-10 17:05:10 +0200

    Once again

---
+++ Twitter_Bot.py
@@ -0,0 +1,9 @@
+import twitter
+
+api = twitter.Api(consumer_key='-',
+                  consumer_secret='-',
+                  access_token_key='-',
+                  access_token_secret='-')
+
+status = api.PostUpdate('Ten post został utworzony za pomocą aplikacji napisanej w języku Python!')
+print(status.text)
